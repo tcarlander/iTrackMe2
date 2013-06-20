ccd01471d62778c6bc071b754941cd60d5816e4f
Tobias Carlander <tobias.carlander@wfp.org>
2013-06-20 10:04:05 +0400

    Modernize Code

--- iTrackMe2/MainViewController.m
+++ iTrackMe2/MainViewController.m
@@ -264,7 +264,7 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     BOOL saved = NO;
     [picker dismissModalViewControllerAnimated:YES];
-    UIImage * myImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImage * myImage = info[UIImagePickerControllerOriginalImage];
     if (myImage) {
         saved = [self pushImageToServer:myImage];
          NSLog(@"Popped %c", saved);
@@ -299,15 +299,15 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
     CLLocationCoordinate2D coordinate = [location coordinate];
     
     Location *dLocation = (Location *)[NSEntityDescription insertNewObjectForEntityForName:@"Location" inManagedObjectContext:[self managedObjectContext]];
-    [dLocation setLatitude:[NSNumber numberWithDouble:coordinate.latitude]];
-    [dLocation setLongitude:[NSNumber numberWithDouble:coordinate.longitude]];
+    [dLocation setLatitude:@(coordinate.latitude)];
+    [dLocation setLongitude:@(coordinate.longitude)];
     [dLocation setDateOccured:[NSDate date]];
-    [dLocation setAltitude:[NSNumber numberWithDouble:location.altitude]];
-    [dLocation setAngle:[NSNumber numberWithDouble:location.course]];
+    [dLocation setAltitude:@(location.altitude)];
+    [dLocation setAngle:@(location.course)];
     [dLocation setComment:@""];
     [dLocation setIconID:@"1"];
-    [dLocation setSpeed:[NSNumber numberWithDouble:location.speed]];
-    [dLocation setUploaded:[NSNumber numberWithInt:0]];
+    [dLocation setSpeed:@(location.speed)];
+    [dLocation setUploaded:@0];
     
     NSError *error = nil;
     if (![[self managedObjectContext] save:&error]) {
@@ -338,7 +338,7 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
     {
         if([self pushObject:dLocation])
         {
-            [dLocation setUploaded:[NSNumber numberWithInt:1]];
+            [dLocation setUploaded:@1];
         }
     }
 
